eb464b721eba92495341308e447baaa4afa74149
Jason Haug <jason.haug@student.ufv.ca>
2025-12-11 10:14:04 -0800

    File error fixes

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,24 +1,6 @@
 extends Control
 
-<<<<<<< HEAD
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
-=======
 func _process(_delta: float) -> void:
 	pass
 
@@ -82,4 +64,3 @@ func _on_resolution_selected(index):
 	var screen_center = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
 	var window_size = DisplayServer.window_get_size()
 	DisplayServer.window_set_position(screen_center - window_size / 2)
->>>>>>> GUI-Controls-Options
