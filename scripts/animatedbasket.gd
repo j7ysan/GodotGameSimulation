5de9107fba330f25806baf11ebe9010b92168330
j2ysan <jeszaree@gmail.com>
2025-12-12 19:43:52 -0800

    3danimation play and sound on touch added
    past base assignment requirement achieved

--- scripts/animatedbasket.gd
+++ scripts/animatedbasket.gd
@@ -3,11 +3,14 @@ extends Node3D
 
 # Onready extension
 @onready var anim_basket = $AnimationPlayer
+@onready var sound_basket = $AudioStreamPlayer3D
+@onready var timer_basket = $Timer
 
-# Attempting to create a function that allocates the function playing for each time the ball touches the area set.
-func _on_body_entered(_touch):
-	_touch = CollisionShape3D
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
 	anim_basket.play("BonesAction_001")
+	sound_basket.play()
+	timer_basket.start()
 	
 func _ready():
 	# At _ready the animation plays as a basic 'test' that our rigged object is working and our 3d animation is working.
