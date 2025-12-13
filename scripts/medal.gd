20cdd0e4720d48a94176f11cdb6d7f3b8917528a
j2ysan <jeszaree@gmail.com>
2025-12-12 19:56:35 -0800

    finalization of game level elements

--- scripts/medal.gd
+++ scripts/medal.gd
@@ -2,16 +2,18 @@ extends Area3D
 
 const ROT_SPEED = 360 #degree of rotation per frame
 @export var hud : CanvasLayer 
+@onready var medal_sound = $AudioStreamPlayer3D
 
 func _process(delta):
 	rotate_y(deg_to_rad(ROT_SPEED * delta))
 
 func _on_body_entered(_body):
 	Global.medals += 1
-	hud.get_node("MedalsLabel").text = str(Global.medals) + " / 3"
+	hud.get_node("MedalsLabel").text = str(Global.medals) + " / 12"
 	set_collision_layer_value(3, false)
 	set_collision_mask_value(1, false)
 	$AnimationPlayer.play("bounce")
+	medal_sound.play()
 
 
 func reset():
